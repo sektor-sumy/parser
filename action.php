e0ead55534ed7bd3f90bbbfc730c6365d4f2375d
Alexander <aeonrush@live.ru>
2016-02-19 14:30:02 +0300

    Добавлена возможность загружать файл со списком ID'ов пользователей [HACK]

--- action.php
+++ action.php
@@ -1,7 +1,13 @@
 <?php
-if (!empty($_POST['search'])) {
-	system('php '.__DIR__.'/api.php search:audio '.$_POST['search'].' > '.__DIR__.'/result &', $retval);
-	echo $retval.' Поиск начался';
-} else {
-	echo 'Ключевое слово не найдено';
-}
+	if (!empty($_POST['search'])) {
+		// HACK Ой-ой.. ну ладно..
+		if(!empty($_FILES) && $_FILES['ids']['error'] == UPLOAD_ERR_OK) {
+			file_put_contents(__DIR__.'/in/in.txt', file_get_contents($_FILES['ids']['tmp_name']));
+		};
+		
+		system('php '.__DIR__.'/api.php search:audio '.$_POST['search'].' > '.__DIR__.'/result &', $retval);
+		echo $retval.' Поиск начался';
+
+	} else {
+		echo 'Ключевое слово не найдено';
+	};
